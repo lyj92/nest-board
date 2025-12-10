64d7f0ef21afb3324726f85eb783361f835eae8f
lyj92 <qlsek922@naver.com>
2025-12-10 13:19:22 +0900

    fix / 회원 생성 시 암호화 로직 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
 } from "@nestjs/common";
 import { User } from "./user.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
+import * as bcrypt from "node_modules/bcryptjs";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -20,21 +21,26 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashPassword,
     });
 
     try {
       await this.save(user);
     } catch (error) {
-      if (error.code === "23505") {
+      if (
+        error &&
+        typeof error === "object" &&
+        "code" in error &&
+        (error as { code: string }).code === "23505"
+      ) {
         throw new ConflictException("이미 존재하는 아이디 입니다.");
-      } else {
-        throw new InternalServerErrorException();
       }
-
-      console.log(error, "error");
+      throw new InternalServerErrorException();
     }
   }
 }
