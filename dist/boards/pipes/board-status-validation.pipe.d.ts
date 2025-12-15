f8fd7f57624a133336808abaa1935a57e84840d1
lyj92 <qlsek922@naver.com>
2025-12-15 16:04:30 +0900

    feat / dist 빌드 폴더 제외 처리

--- dist/boards/pipes/board-status-validation.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from "@nestjs/common";
-import { BoardStatus } from "../board-status-enum";
-export declare class BoardStatusValidationPipe implements PipeTransform {
-    readonly StatusOptions: BoardStatus[];
-    transform(value: string): string;
-    private isStatusValid;
-}
